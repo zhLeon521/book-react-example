5d64e2e8156f1bb57823ca7d56adfe4ba80b7315
zhleon <zholeon@qq.com>
2021-11-30 21:39:35 +0800

    props

--- src/index.js
+++ src/index.js
@@ -1,72 +1,52 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// 无状态组件
-// always return JSX
-
-// function Geeting() {
-//   return <h2>
-//     This is my first components
-//   </h2>
-// }
-
-// const Geeting = () => {
-//   return React.createElement('h2', {}, 'hello world')
-// }
+import './index.css'
 
 
-
-// function Geeting() {
-//   return <div>
-//     <h2>
-//       This is my first components
-//     </h2>
-//   </div>
-// }
-
-// const Geeting = () => {
-//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'))
-// }
-
-
-// function Geeting() {
-//   return <div classNAme="">
-//     <h2>
-//       This is my first components
-//     </h2>
-//     <ul>
-//       <li><a href='#'>hello world</a></li>
-//       <li><a href='#'>hello react</a></li>
-//       <img src='' alt="" />
-//       <input type="text" name='' id=''/>
-//     </ul>
-//   </div>
-// }
-
-
-function Geeting() {
-  return <div classNAme="">
-    <Person />
-    <Message/>
-    
-
-  </div>
+const firstbook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/510NuU01osL._AC_UL200_SR200,200_.jpg",
+  author: 'Brené Brown',
+  title: 'Atlas of the Heart: Mapping Meaningful'
+}
+const secondbook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL200_SR200,200_.jpg",
+  author: 'Colleen Hoover',
+  title: 'It Ends With Us: A Novel'
 }
 
-// 没有括号就不用return
-const Person = () => <h2>This is my first components</h2>
-// 有括号就要return
-const Message = () => {
+const BookList = () => {
   return (
-  <ul>
-    <li><a href='#'>hello world</a></li>
-    <li><a href='#'>hello react</a></li>
-    <img src='' alt="" />
-    <input type="text" name='' id=''/>
-  </ul>
+    <section className="booklist">
+      <Book
+        img={firstbook.img}
+        title={firstbook.title}
+        author={firstbook.author.toUpperCase()}
+      />
+      <Book
+        img={secondbook.img}
+        title={secondbook.title}
+        author={secondbook.author}
+      />
+
+    </section>
+  )
+}
+
+const Book = (props) => {
+  console.log(props)
+
+  return (
+    <article className='book'>
+      <img src={props.img} />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <p>{9 + 9}</p>
+    </article>
   )
 
 }
 
-ReactDOM.render(<Geeting/>, document.getElementById('root'))
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
 
