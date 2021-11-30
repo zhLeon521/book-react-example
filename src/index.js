ed75c74d6b2a1df6eaf2e541f721b03952a75065
zhleon <zholeon@qq.com>
2021-11-30 11:39:55 +0800

    书写规范

--- src/index.js
+++ src/index.js
@@ -29,18 +29,43 @@ import ReactDOM from 'react-dom';
 // }
 
 
+// function Geeting() {
+//   return <div classNAme="">
+//     <h2>
+//       This is my first components
+//     </h2>
+//     <ul>
+//       <li><a href='#'>hello world</a></li>
+//       <li><a href='#'>hello react</a></li>
+//       <img src='' alt="" />
+//       <input type="text" name='' id=''/>
+//     </ul>
+//   </div>
+// }
+
+
 function Geeting() {
   return <div classNAme="">
-    <h2>
-      This is my first components
-    </h2>
-    <ul>
-      <li><a href='#'>hello world</a></li>
-      <li><a href='#'>hello react</a></li>
-      <img src='' alt="" />
-      <input type="text" name='' id=''/>
-    </ul>
+    <Person />
+    <Message/>
+    
+
   </div>
+}
+
+// 没有括号就不用return
+const Person = () => <h2>This is my first components</h2>
+// 有括号就要return
+const Message = () => {
+  return (
+  <ul>
+    <li><a href='#'>hello world</a></li>
+    <li><a href='#'>hello react</a></li>
+    <img src='' alt="" />
+    <input type="text" name='' id=''/>
+  </ul>
+  )
+
 }
 
 ReactDOM.render(<Geeting/>, document.getElementById('root'))
